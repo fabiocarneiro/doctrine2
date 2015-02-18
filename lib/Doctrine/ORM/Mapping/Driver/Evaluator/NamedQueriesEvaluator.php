1a7206a53e2bab810d5f478c215b9c393d941eac
fabiocarneiro <fahecs@gmail.com>
2015-02-18 01:13:28 -0200

    fix NamedQueriesEvaluator

--- lib/Doctrine/ORM/Mapping/Driver/Evaluator/NamedQueriesEvaluator.php
+++ lib/Doctrine/ORM/Mapping/Driver/Evaluator/NamedQueriesEvaluator.php
@@ -3,22 +3,28 @@
 namespace Doctrine\ORM\Mapping\Driver\Evaluator;
 
 use Doctrine\Common\Persistence\Mapping\ClassMetadata;
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
+use InvalidArgumentException;
 
 
 /**
  * @author    Fábio Carneiro <fahecs@gmail.com>
  * @license   MIT
  */
-class NamedQueryEvaluator implements EvaluatorInterface
+class NamedQueriesEvaluator implements EvaluatorInterface
 {
     /**
      * {@inheritDoc}
-     * @param array         $element
-     * @param ClassMetadata $metadata
+     * @param array             $element
+     * @param ClassMetadataInfo $metadata
      * @return void
      */
     public function evaluate(array $element, ClassMetadata $metadata)
     {
+        if ( ! $metadata instanceof ClassMetadataInfo) {
+            throw new InvalidArgumentException('Metadata must be a instance of ClassMetadataInfo');
+        }
+
         if ( ! isset($element['namedQueries'])) {
             return;
         }
