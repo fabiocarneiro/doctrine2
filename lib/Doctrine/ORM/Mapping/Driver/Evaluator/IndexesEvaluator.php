5015655dd7c594d51e1ce096a071a57fe10d74e1
fabiocarneiro <fahecs@gmail.com>
2015-02-18 01:11:48 -0200

    fix IndexesEvaluator

---
+++ lib/Doctrine/ORM/Mapping/Driver/Evaluator/IndexesEvaluator.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Doctrine\ORM\Mapping\Driver\Evaluator;
+
+use Doctrine\Common\Persistence\Mapping\ClassMetadata;
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
+
+/**
+ * @user    Fábio Carneiro <fahecs@gmail.com>
+ * @license MIT
+ */
+class IndexesEvaluator implements EvaluatorInterface
+{
+    /**
+     * {@inheritDoc}
+     * @param array             $element
+     * @param ClassMetadataInfo $metadata
+     * @return void
+     */
+    public function evaluate(array $element, ClassMetadata $metadata)
+    {
+        if ( ! $metadata instanceof ClassMetadataInfo) {
+            throw new InvalidArgumentException('Metadata must be a instance of ClassMetadataInfo');
+        }
+
+        if ( ! isset($element['indexes'])) {
+            return;
+        }
+
+        foreach ($element['indexes'] as $name => $indexYml) {
+            if ( ! isset($indexYml['name'])) {
+                $indexYml['name'] = $name;
+            }
+            if (is_string($indexYml['columns'])) {
+                $index = [
+                    'columns' => array_map(
+                        'trim',
+                        explode(',', $indexYml['columns'])
+                    )
+                ];
+            } else {
+                $index = ['columns' => $indexYml['columns']];
+            }
+            if (isset($indexYml['flags'])) {
+                if (is_string($indexYml['flags'])) {
+                    $index['flags'] =
+                        array_map('trim', explode(',', $indexYml['flags']));
+                } else {
+                    $index['flags'] = $indexYml['flags'];
+                }
+            }
+            if (isset($indexYml['options'])) {
+                $index['options'] = $indexYml['options'];
+            }
+            $metadata->table['indexes'][$indexYml['name']] = $index;
+        }
+    }
+}
